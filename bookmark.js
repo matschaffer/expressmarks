cc790f638006d6e8bda32c0116f11504b3bd011b
Mat Schaffer <mat.schaffer@gmail.com>
2010-03-30 08:41:04 -0400

    Updated Bookmark.create to also put the resulting bookmark into redis tag sets.

--- bookmark.js
+++ bookmark.js
@@ -1,20 +1,42 @@
 require('ext');
 
 var Riak = require('riak-node'),
+    Redis = require('redisclient'),
     sys = require('sys'),
     Bookmark = exports.Bookmark = function() {};
 
 Bookmark.db = new Riak.Client();
+Bookmark.index = new Redis.Client();
 
 Bookmark.bucket = "bookmarks";
 
 Bookmark.create = function(user, params, fn) {
+  var key, tag, self = this,
+      bookmark = new Bookmark();
+
   params.user = user;
+  Ext.extend(bookmark, params);
   
   this.db.save(this.bucket, null, params)(function(err, resp) {
-    var key = resp.headers.location.split('/').last;
+    key = resp.headers.location.split('/').last;
     
-    if (fn) fn(key);
+    self.index.connect(function() {
+      params.tags.push("by:user");
+
+      function storeTags(tags) {
+        if (tags.isEmpty) {
+          self.index.quit();
+          if (fn) fn(key);
+        } else {
+          tag = tags.first;
+          self.index.rpush(tag, key, function() {
+            sys.puts("rpush'd " + tag);
+            storeTags(tags.drop(1));
+          });
+        }
+      }
+      storeTags(params.tags);
+    });
   });
 };
 
--- integration/storage.js
+++ integration/storage.js
@@ -9,9 +9,11 @@ Bookmark.create(user, {
   title: "Mat Schaffer's blog",
   notes: "My personal blog. Not sure why I'd bookmark it.",
   tags: ["programming", "ruby", "javascript", "awesome"]
-}, function(key) {
-  sys.p("Created " + key);
-  Bookmark.findByUser(user, function(bookmarks) {
-    sys.p(bookmarks);
+}, function() {
+  sys.puts("Finished storage with these args to callback.");
+  sys.p(arguments);
+  Bookmark.findByUser(user, function() {
+    sys.puts("Finished lookup with these args");
+    sys.p(arguments);
   });
 });
