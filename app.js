fdfa79857cf260887ae1a201e1bc142d0a51850f
Mat Schaffer <mat.schaffer@gmail.com>
2010-03-31 23:38:18 -0400

    Mapped up some routes for user and general tag queries.

--- app.js
+++ app.js
@@ -12,13 +12,23 @@ var redis = require('redisclient'),
     sys = require('sys'),
     Bookmark = require('bookmark').Bookmark;
 
-get('/:user?/*', function(user, tagPath) {
+function renderTags(tagPath) {
   var self = this,
       tags = tagPath.split('/')
 
-  Bookmark.findByTags(tags.concat("by:" + user), function(bookmarks) {
+  Bookmark.findByTags(tags, function(bookmarks) {
     self.render('index.haml.html', { locals: { bookmarks: bookmarks }})
   })
+}
+
+function renderUserTags(user, tagPath) {
+  renderTags.apply(this, [tagPath + "/by:" + user]);
+}
+
+get('/favicon.ico', function(file){
+  this.halt(404)
 })
+get('/tags/*', renderTags)
+get('/:user?/*', renderUserTags)
 
 run()
