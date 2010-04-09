a5b60308cb60aee53bb1ca8425ed3fd9d17266fe
Mat Schaffer <mat.schaffer@gmail.com>
2010-04-08 20:00:26 -0400

    Added some handlers to show bookmarks.

--- app.js
+++ app.js
@@ -6,29 +6,19 @@ configure(function() {
   set('root', __dirname);
 })
 
-var redis = require('redisclient'),
-    riak = require('riak-node'),
-    handlers = require('./handlers'),
-    sys = require('sys'),
-    Bookmark = require('bookmark').Bookmark;
-
-function renderTags(tagPath) {
-  var self = this,
-      tags = tagPath.split('/')
-
-  Bookmark.findByTags(tags, function(bookmarks) {
-    self.render('index.haml.html', { locals: { bookmarks: bookmarks }})
-  })
-}
-
-function renderUserTags(user, tagPath) {
-  renderTags.apply(this, [tagPath + "/by:" + user]);
-}
+var redis = require('redisclient')
+   ,riak = require('riak-node')
+   ,handlers = require('./handlers')
+   ,sys = require('sys')
 
 get('/favicon.ico', function(file){
   this.halt(404)
 })
-get('/tags/*', renderTags)
-get('/:user?/*', renderUserTags)
+
+get('/tags/*', handlers.renderTags)
+
+get('/:user?/*', function(user, tagPath) {
+  handlers.tags.apply(this, [tagPath + "/by:" + user])
+})
 
 run()
--- handlers.js
+++ handlers.js
@@ -1,5 +1,15 @@
-var sys = require('sys');
+var sys = require('sys')
+   ,Bookmark = require('bookmark').Bookmark
 
 exports.get = function () {
   this.render("index.haml.html");
 };
+
+exports.tags = function (tagPath) {
+  var self = this,
+      tags = tagPath.split('/')
+
+  Bookmark.findByTags(tags, function(bookmarks) {
+    self.render('index.haml.html', { locals: { bookmarks: bookmarks }})
+  })
+}
